35c070c9421ac8e9c676dd860bfed5ec75f90d38
Onyi Cho <onyicho@gmail.com>
2019-05-24 16:32:28 -0700

    Solve hashing

--- hash_map_lru_cache/skeleton/lib/p02_hashing.rb
+++ hash_map_lru_cache/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    hashcode = 0
+    self.each_with_index do |ele, i|
+      hashcode += ele.hash * i.hash
+    end
+    hashcode
   end
 end
 
 class String
   def hash
+    hashcode = 0
+    self.each_char.with_index do |char, i|
+      hashcode += char.ord * i.hash
+    end
+    hashcode
   end
 end
 
@@ -16,6 +26,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hashcode = 0
+
+    self.each_key do |key|
+      hashcode += self[key].hash 
+    end
+    hashcode
   end
 end
