a19d2459c82f5d1000e798fc1183dda26905e088
Onyi Cho <onyicho@gmail.com>
2019-05-24 16:57:55 -0700

    Solve p04_hash_set.rb

--- hash_map_lru_cache/skeleton/lib/p03_hash_set.rb
+++ hash_map_lru_cache/skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count >= num_buckets
+    unless include?(key)
+      self[key] << key 
+      @count += 1 
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key) 
+      @count -= 1
+    end  
   end
 
   private
 
-  def [](num)
+  def [](val)
     # optional but useful; return the bucket corresponding to `num`
+    @store[val.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +37,14 @@ class HashSet
   end
 
   def resize!
+    size = num_buckets
+    size *= 2
+    old_store = @store
+    @store = Array.new(size) { Array.new }
+    old_store.each do |arr|
+      arr.each do |ele|
+        self[ele] << ele
+      end
+    end
   end
 end
