2d16147d1a8908e839147fd1103ec1068b989b87
Onyi Cho <onyicho@gmail.com>
2019-05-24 23:32:40 -0700

    Add latest attempt for the Linked List part

--- hash_map_lru_cache/skeleton/lib/p04_linked_list.rb
+++ hash_map_lru_cache/skeleton/lib/p04_linked_list.rb
@@ -16,11 +16,21 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    prev_node = @prev 
+    next_node = @next
+    prev_node.next = next_node
+    next_node.prev = prev_node
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new()
+    @tail = Node.new()
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,21 +39,31 @@ class LinkedList
   end
 
   def first
+
   end
 
   def last
   end
 
   def empty?
+    @prev.nil? && @next.nil?
   end
 
   def get(key)
+    @val if @key == key
   end
 
   def include?(key)
+    if self.key == key
+      return self
+    else
+      @next.include?(key)
+    end
+    
   end
 
   def append(key, val)
+    
   end
 
   def update(key, val)
@@ -53,6 +73,10 @@ class LinkedList
   end
 
   def each
+    if @next.nil
+      return []
+    end
+    @next.each << @val
   end
 
   # uncomment when you have `each` working and `Enumerable` included
